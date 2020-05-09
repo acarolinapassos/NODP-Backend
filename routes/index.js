34db079c93e72325fe55b2e9f055896accd0c2c1
Alessandro Menezes <alessandroadm@live.com>
2020-05-08 21:35:20 -0300

    teste de branch

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(`Deu certo!`);
+  res.send(`Deu certo...`);
 });
 
 module.exports = router;
