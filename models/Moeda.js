90794caf39715201c1cebb5e79ba6856e51a4165
Alessandro Menezes <alessandroadm@live.com>
2020-05-09 21:25:19 -0300

    feito classe moedas

---
+++ models/Moeda.js
@@ -0,0 +1,48 @@
+//https://sequelize.org/v5/manual/data-types.html
+const Moeda = (sequelize, Datatypes) => {
+
+  let moeda = sequelize.define(
+
+    'Moeda',
+    {
+      id: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      usuario_id: {
+        type: Datatypes.INTEGER,
+        primaryKey: false,
+        autoIncrement: false,
+        allowNull: false,
+      },
+      remetente_id: {
+        type: Datatypes.INTEGER,
+        primaryKey: false,
+        autoIncrement: false,
+        allowNull: false,
+      },
+      qtd_moedas: {
+        type: Datatypes.INTEGER,
+        primaryKey: false,
+        autoIncrement: false,
+        allowNull: false,
+      },
+      data_hora: {
+        type: Datatypes.DATE,
+        primaryKey: false,
+        autoIncrement: false,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'moedas',
+      timestamps: false
+    }
+  );
+
+  return moeda;
+};
+
+module.exports = Moeda;
