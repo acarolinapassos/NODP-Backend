85a34dd4eb438204f8dae292afd796848d4bdb75
Alessandro Menezes <alessandroadm@live.com>
2020-05-09 22:53:14 -0300

    finalizado bateria de testes para transações de moedas

--- testes/moedaTeste.js
+++ testes/moedaTeste.js
@@ -1,44 +1,58 @@
 const { sequelize, Moeda } = require('../models');
 
-
-Moeda.findAll().then(
-  result => {
-    console.log(result.map(u => u.toJSON()));
-    sequelize.close();
-  }
-);
-
-let user = { usuario_id: 1, remetente_id: 2, qtd_moedas: 3 };
-Moeda.create(user).then(
-  result => {
-    console.log(result);
-  }
-);
-
-
-  
-/*
+//-----------------------------------------------------------------------
+//Listar transações 
 async function listar() {
   try {
-    let lista = await Moeda.findAll(
-      {
-        include: [{
-          model: Moeda,
-          as: 'perfis',
-          include: 'usuarios'
-        }]
-      });
+    let lista = await Moeda.findAll();
     for (let moeda of lista) {
-      console.log('---------------------------------------');
-      console.log(`Nome: ${moeda.perfis[0].nome}`);
-      console.log(`Email: ${moeda.email}`);
-      console.log(`Senha: ${moeda.senha}`);
-      console.log('---------------------------------------');
+      console.log(`DE: User ${moeda.remetente_id} -> PARA: User ${moeda.usuario_id}`);
+      console.log(`${moeda.qtd_moedas} Moedas`);
+      console.log(`Em: ${moeda.data_hora}`);
+      console.log('---------');
     }
   } catch (error) {
-    console.log(error);
+    
   }
   sequelize.close();
 }
-*/
-//listar();
+
+//-----------------------------------------------------------------------
+//Realizar uma transação
+async function realizarTransacao() {
+  try {
+    let qtd = Math.round(Math.random() * (10 - 1) + 1);
+    let user1 = Math.round(Math.random() * (4 - 1) + 1);
+    let user2 = Math.round(Math.random() * (4 - 1) + 1);
+    (user1 == user2) ? user1 = user1 + 1 : '';
+    let user = { usuario_id: user1, remetente_id: user2, qtd_moedas: qtd };
+    let resultado = await Moeda.create(user); 
+    console.log('---------');
+    console.log('Foi realizado uma nova operação com sucesso!');
+    console.log(resultado.dataValues);
+  } catch (error) {
+    console.log(error);
+  } 
+  sequelize.close();
+}
+
+//-----------------------------------------------------------------------
+//Buscar uma transação pelo ID
+async function buscar() {
+  try {
+    let id = Math.round(Math.random() * (12 - 1) + 1);
+    let resultado = await Moeda.findByPk(id);
+    console.log('---------');
+    console.log(`Buscando pelo ID: ${id}`);
+    console.log(resultado.dataValues);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//-----------------------------------------------------------------------
+//TESTES A SEREM EXECUTADO 
+realizarTransacao();
+listar();
+buscar();
+
