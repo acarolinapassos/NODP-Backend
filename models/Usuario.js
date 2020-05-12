e8d153ffc7d919c5e8e619970e07272b4c661ae7
EricaSugui <ericasugui@gmail.com>
2020-05-11 22:48:06 -0300

    erica adicionando models Usuario e Perfil

---
+++ models/Usuario.js
@@ -0,0 +1,29 @@
+//https://sequelize.org/v5/manual/data-types.html
+const Usuario = (sequelize, DataTypes) => {
+    let usuarios = sequelize.define(
+        'Usuario',
+        {
+            id: {
+                type: Datatypes.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+            },
+            email: Datatypes.STRING(80),
+            senha: Datatypes.STRING(250),
+            admin: Datatypes.BOOLEAN,
+            ativo: Datatypes.BOOLEAN
+        },
+        {
+            tableName: 'usuarios',
+            timestamps: false
+        }
+    );
+
+    usuarios.associate = (models) => {
+        usuarios.belongsTo(models.Perfil, {foreignKey:'usuario_id', as: 'perfil'});
+    }
+
+    return usuarios;
+};
+module.exports = Usuario;
