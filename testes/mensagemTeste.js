1f9dc21edb1526d00c37f1563433dfebcf4786bf
Alessandro Menezes <alessandroadm@live.com>
2020-05-11 21:35:30 -0300

    ajustando mensagem

--- testes/mensagemTeste.js
+++ testes/mensagemTeste.js
@@ -5,11 +5,11 @@ async function enviarMensagem() {
         let usuarioId = Math.round(Math.random() * (4 - 1) + 1);
         let destinatarioId = Math.round(Math.random() * (4 - 1) + 1);
         (usuarioId == destinatarioId) ? usuarioId = usuarioId + 1 : '';
-        let objeto = {usuario_id: usuarioId, destinatario_id: destinatarioId, mensagem: 'Testte'}
+        let objeto = { usuario_id: usuarioId, destinatario_id: destinatarioId, mensagem: 'Testte' };
         let enviar = await Mensagem.create(objeto); 
-        console.log(enviar)
+        console.log(enviar);
     } catch(error){
-        console.log(error)
+        console.log(error);
     }
     sequelize.close();
 }
