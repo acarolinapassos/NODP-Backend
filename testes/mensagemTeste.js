c4492ab8c005045ab2742e1c0c4c3234dfe420d3
André Oliveira <andreluisoliveira013@gmail.com>
2020-05-12 08:22:44 -0300

    Adicionando funcao de listagem de mensagens

--- testes/mensagemTeste.js
+++ testes/mensagemTeste.js
@@ -5,13 +5,23 @@ async function enviarMensagem() {
         let usuarioId = Math.round(Math.random() * (4 - 1) + 1);
         let destinatarioId = Math.round(Math.random() * (4 - 1) + 1);
         (usuarioId == destinatarioId) ? usuarioId = usuarioId + 1 : '';
-        let objeto = { usuario_id: usuarioId, destinatario_id: destinatarioId, mensagem: 'Testte' };
+        let objeto = {usuario_id: usuarioId, destinatario_id: destinatarioId, mensagem: 'Testte'}
         let enviar = await Mensagem.create(objeto); 
-        console.log(enviar);
+        console.log(enviar)
     } catch(error){
-        console.log(error);
+        console.log(error)
     }
     sequelize.close();
 }
 
-enviarMensagem();
+async function listarMensagens() {
+    try {
+        let listar = await Mensagem.findAll()
+        console.log(listar)
+    } catch(error){
+        console.log(error)
+    }
+    sequelize.close();
+}
+
+listarMensagens();
