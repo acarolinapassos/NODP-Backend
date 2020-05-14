a7155788dbce6a6bfc715563098d0ae4a2468ab2
EricaSugui <ericasugui@gmail.com>
2020-05-14 19:50:44 -0300

    erica seeder comentarios

--- seeders/20200514032040-create_table_comentarios.js
+++ seeders/20200514032040-create_table_comentarios.js
@@ -3,24 +3,49 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+      texto: Sequelize.STRING(250),
+      usuario_id: Sequelize.INTEGER,
+      post_id: Sequelize.INTEGER
+    */  
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      return queryInterface.bulkInsert(
+        'comentarios',
+        [
+          {
+            texto: 'Tenha um lindo dia!',
+            usuario_id: 1,
+            post_id: 1
+          },
+          {
+            texto: 'Cuidem-se e lavem as mãos.',
+            usuario_id: 2,
+            post_id: 1
+          },
+          {
+            texto: 'Alguém poderia me ajudar a subir o servidor?',
+            usuario_id: 3,
+            post_id: 2
+          },
+          {
+            texto: 'Claro! do que vc precisa? Eu tenho uma escada.',
+            usuario_id: 4,
+            post_id: 2
+          },
+          {
+            texto: 'Só se sua escada vier pela porta 3000',
+            usuario_id: 5,
+            post_id: 2
+          },
+          {
+            texto: 'Boa noite! Alguém a fim de conversar um pouco?',
+            usuario_id: 3,
+            post_id: 3
+          }
+        ],
+        {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      return queryInterface.bulkDelete('comentarios', null, {});
   }
 };
