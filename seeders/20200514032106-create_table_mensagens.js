1f645778cd7c085071acfe2b9082aa07611429a4
Alessandro Menezes <alessandroadm@live.com>
2020-05-14 19:41:57 -0300

    feito seeders

--- seeders/20200514032106-create_table_mensagens.js
+++ seeders/20200514032106-create_table_mensagens.js
@@ -3,24 +3,102 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+    Add altering commands here.
+    Return a promise to correctly handle asynchronicity.
+    
+    Example:
     */
+    return queryInterface.bulkInsert('mensagens', [
+      {
+        usuario_id:1,
+        destinatario_id:2, 
+        mensagem:'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 2,
+        mensagem: 'Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },      
+      {
+        usuario_id: 2,
+        destinatario_id: 1,
+        mensagem: 'pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 3,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 4,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 5,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 6,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 7,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 1,
+        destinatario_id: 8,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 2,
+        destinatario_id: 3,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 3,
+        destinatario_id: 4,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 3,
+        destinatario_id: 5,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 5,
+        destinatario_id: 1,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 5,
+        destinatario_id: 2,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 6,
+        destinatario_id: 2,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      },
+      {
+        usuario_id: 4,
+        destinatario_id: 2,
+        mensagem: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.'
+      }
+    ], {});
   },
-
+  
   down: (queryInterface, Sequelize) => {
     /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
+    Add reverting commands here.
+    Return a promise to correctly handle asynchronicity.
+    
+    Example:
     */
+    return queryInterface.bulkDelete('mensagens', null, {});
   }
 };
--- seeders/20200514032251-create_table_moedas.js
+++ seeders/20200514032251-create_table_moedas.js
@@ -3,24 +3,92 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+    Add altering commands here.
+    Return a promise to correctly handle asynchronicity.
+    
+    Example:
     */
+    return queryInterface.bulkInsert('moedas', [
+      {
+      usuario_id: 1,
+      remetente_id:2,
+      qtd_moedas:10
+      },
+      {
+        usuario_id: 2,
+        remetente_id: 1,
+        qtd_moedas: 2
+      },
+      {
+        usuario_id: 1,
+        remetente_id: 3,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 1,
+        remetente_id: 4,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 4,
+        remetente_id: 1,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 1,
+        remetente_id: 5,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 1,
+        remetente_id: 6,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 6,
+        remetente_id: 7,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 5,
+        remetente_id: 6,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 6,
+        remetente_id: 7,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 1,
+        remetente_id: 2,
+        qtd_moedas: 1
+      },
+      {
+        usuario_id: 2,
+        remetente_id: 1,
+        qtd_moedas: 1
+      },
+{
+        usuario_id: 1,
+        remetente_id: 8,
+        qtd_moedas: 7
+      },
+      {
+        usuario_id: 8,
+        remetente_id: 9,
+        qtd_moedas: 2
+      }
+    ], {});
   },
-
+  
   down: (queryInterface, Sequelize) => {
     /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
+    Add reverting commands here.
+    Return a promise to correctly handle asynchronicity.
+    
+    Example:
     */
+    return queryInterface.bulkDelete('moedas', null, {});
   }
 };
