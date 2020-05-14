d4693f83f15f36e8b2157784d20aac30b1dd17e0
Alessandro Menezes <alessandroadm@live.com>
2020-05-14 19:51:58 -0300

    seeder de aulas

--- seeders/20200514032127-create_table_aulas_ministradas.js
+++ seeders/20200514032127-create_table_aulas_ministradas.js
@@ -3,24 +3,70 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+    Add altering commands here.
+    Return a promise to correctly handle asynchronicity.
+    
+    Example:
     */
+    return queryInterface.bulkInsert('aulas_ministradas', [
+      {
+        usuario_id:1,
+        aluno_id:2,
+        titulo_aula:'Javascript',
+        descricao:'Aula prática de Javascript',
+        qnt_moedas:2,
+        duracao_minutos:30
+      },
+      {
+        usuario_id:1,
+        aluno_id:3,
+        titulo_aula:'Javascript',
+        descricao:'Aula prática de Javascript',
+        qnt_moedas:2,
+        duracao_minutos:30
+      },
+      {
+        usuario_id:2,
+        aluno_id:1,
+        titulo_aula:'PHP',
+        descricao:'Aula objetiva de PHP',
+        qnt_moedas:2,
+        duracao_minutos:30
+      },
+      {
+        usuario_id:3,
+        aluno_id:2,
+        titulo_aula:'Culinária',
+        descricao:'Lorem ipsum na área quando falta palavras',
+        qnt_moedas:2,
+        duracao_minutos:30
+      },
+      {
+        usuario_id: 4,
+        aluno_id: 2,
+        titulo_aula: 'Javascript',
+        descricao: 'Aula prática de Javascript',
+        qnt_moedas: 2,
+        duracao_minutos: 30
+      },
+      {
+        usuario_id: 5,
+        aluno_id: 2,
+        titulo_aula: 'Javascript',
+        descricao: 'Aula prática de Javascript',
+        qnt_moedas: 2,
+        duracao_minutos: 30
+      }
+    ], {});
   },
-
+  
   down: (queryInterface, Sequelize) => {
     /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
+    Add reverting commands here.
+    Return a promise to correctly handle asynchronicity.
+    
+    Example:
     */
+   return queryInterface.bulkDelete('aulas_ministradas', null, {});
   }
 };
