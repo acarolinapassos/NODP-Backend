8bf996568ab84a10f1deead5774d41dc5b35d0b4
André Oliveira <andreluisoliveira013@gmail.com>
2020-05-14 16:45:55 -0300

    Adicionando model de interesses

---
+++ models/Interesses.js
@@ -0,0 +1,27 @@
+const Interesses = ( sequelize, Datatypes ) => {
+
+    let interesses = sequelize.define(
+        'Interesses',
+        {
+            id: {
+                type: Datatypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            descricao: {
+                type: Datatypes.STRING(250),
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            }
+        },
+        {
+            tableName: 'interesses',
+            timestamps: false
+        }
+    );
+    return interesses
+}
+
+module.exports = Interesses
