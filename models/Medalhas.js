8ee2b5f6f433cca8a94fa81aee40597f1f2cda18
André Oliveira <andreluisoliveira013@gmail.com>
2020-05-14 17:26:13 -0300

    Adicionando model de medalhas

---
+++ models/Medalhas.js
@@ -0,0 +1,39 @@
+const Medalhas = ( sequelize, Datatypes ) => {
+
+    let medalhas = sequelize.define(
+        'Medalhas',
+        {
+            id: {
+                type: Datatypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            id_post: {
+                type: Datatypes.INTEGER,
+                primaryKey: false,
+                autoIncrement: false,
+                allowNull: false
+            },
+            remetente_id: {
+                type: Datatypes.INTEGER,
+                primaryKey: false,
+                autoIncrement: false,
+                allowNull: false
+            },
+            data_hora:{
+                type: Datatypes.DATE,
+                primaryKey: false,
+                autoIncrement: false,
+                allowNull: true,
+            }
+        },
+        {
+            tableName: 'medalhas',
+            timestamps: false
+        }
+    );
+    return medalhas
+}
+
+module.exports = Medalhas
