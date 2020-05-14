52eb06da5600cc9491a1ed8f44060592bc281ff8
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-13 22:59:14 -0300

    conflito

--- migrations/20200513230508-create_table_cursos.js
+++ migrations/20200513230508-create_table_cursos.js
@@ -7,8 +7,31 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      
     */
+    return queryInterface.createTable('cursos', {
+      id: {
+        type: Datatype.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      descricao: {
+        type: DataTypes.STRING(50),
+        primaryKey: false,
+        autoIncrement: false,
+        allowNull: false
+
+      },
+      usuario_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: false,
+        autoIncrement: false,
+        allowNull: false
+
+      },
+
+    });
   },
 
   down: (queryInterface, Sequelize) => {
