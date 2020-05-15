c647c37a1e047f189e4986e96815e3c06e4905cc
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-15 20:25:27 -0300

    migration usuario

--- migrations/20200513224500-create_table_usuarios.js
+++ migrations/20200513224500-create_table_usuarios.js
@@ -5,27 +5,34 @@ module.exports = {
     return queryInterface.createTable(
       'usuarios',
       { 
-        id: {
-          type: Sequelize.INTEGER,
+        'id': {
+          type: Sequelize.INTEGER(11),
           allowNull: false,
           primaryKey: true,
+          comment: "null",
           autoIncrement: true
         },
-        email: {
-          type:Sequelize.STRING(80),
-          allowNull: false
+        'email': {
+          type: Sequelize.STRING(80),
+          allowNull: false,
+          comment: "null"
         },
-        senha: {
-          type: Sequelize.STRING(255),
-          allowNull: false
+        'senha': {
+          type: Sequelize.STRING(250),
+          allowNull: false,
+          comment: "null"
         },
-        admin: {
-          type: Sequelize.BOOLEAN,
-          allowNull: false
+        'admin': {
+          type: Sequelize.INTEGER(1),
+          allowNull: false,
+          defaultValue: '0',
+          comment: "null"
         },
-        ativo: {
-          type: Sequelize.BOOLEAN,
-          allowNull: false
+        'ativo': {
+          type: Sequelize.INTEGER(1),
+          allowNull: false,
+          defaultValue: '1',
+          comment: "null"
         }
         
       });
