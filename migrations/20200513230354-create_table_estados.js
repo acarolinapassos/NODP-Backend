74d74e522041ed5ec6cbdfe213177153ce71315d
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-15 20:52:51 -0300

    migration estados

--- migrations/20200513230354-create_table_estados.js
+++ migrations/20200513230354-create_table_estados.js
@@ -3,17 +3,24 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.createTable(
-        'estados',
-        {
-          cod_estados: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-          },
-          sigla: Sequelize.STRING(2),
-          nome: Sequelize.STRING(72)
-        });
+        'estados',{
+        cod_estados: {
+          type: DataTypes.INTEGER(11),
+          allowNull: false,
+          primaryKey: true,
+          comment: "null",
+          autoIncrement: true
+        },
+        sigla: {
+          type: DataTypes.STRING(2),
+          allowNull: false,
+          comment: "null"
+        },
+        nome: {
+          type: DataTypes.STRING(72),
+          allowNull: false,
+          comment: "null"
+        }});
   },
 
   down: (queryInterface, Sequelize) => {
