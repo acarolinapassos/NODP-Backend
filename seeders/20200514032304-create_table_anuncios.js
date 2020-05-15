01fc8a85095a0e81e6ce99170a9e34cb86067fe5
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-14 22:33:52 -0300

    seeder anuncios

--- seeders/20200514032304-create_table_anuncios.js
+++ seeders/20200514032304-create_table_anuncios.js
@@ -7,11 +7,33 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
+      return queryInterface.bulkInsert('anuncios', [{
         name: 'John Doe',
         isBetaMember: false
       }], {});
     */
+   return queryInterface.bulkInsert('anuncios', [{
+    descricao: "Não espere para se movimentar, venha para Bluefit" ,
+      
+ },{
+    descricao: "Emagreça com a Helbelife" ,
+    
+  },
+  {
+    descricao: "Hey,mate! venha estudar inglês com a gente." ,
+    
+  },
+  {
+    descricao: "Ingressos disponíveis para show do Sepultura." ,
+    
+  },{
+    descricao: "Curso Data Science por apenas 10 reais." ,
+    
+  },
+  {
+    descricao: "Faça sua pós graduação por apenas 100 reais ao mês ." ,
+    
+  }], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +42,8 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
+     
     */
+   return queryInterface.bulkDelete('anuncios', null, {});
   }
 };
