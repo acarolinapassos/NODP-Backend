5febd463487d4af71fc31615534d73558b2d3f6a
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-16 01:56:20 -0300

    seeders criadas FINALMENTE

--- seeders/20200514032237-create_table_medalhas.js
+++ seeders/20200514032237-create_table_medalhas.js
@@ -5,32 +5,32 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('medalhas', [{
       id_post:1,
-      postagens_id: 2,
+      
       remetente_id: 3,
       data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
       id_post:1,
-      postagens_id: 3,
+      
       remetente_id: 1,
       data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
       id_post:1,
-      postagens_id: 2,
+      
       remetente_id: 1,
       data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
       id_post:1,
-      postagens_id: 1,
+      
       remetente_id: 4,
       data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
       id_post:1,
-      postagens_id: 2,
+      
       remetente_id: 3,
       data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
       id_post:1,
-      postagens_id: 5,
+      
       remetente_id: 4,
       data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }], {});
