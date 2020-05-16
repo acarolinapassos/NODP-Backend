bc0773e5a2b0cb63c70d1feae1f09c202eb6e0a6
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-16 01:14:18 -0300

    seeders

--- seeders/20200514031945-create_table_interesses.js
+++ seeders/20200514031945-create_table_interesses.js
@@ -3,22 +3,28 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('interesses', [
-      {
+      { 
+        id:1,
         descricao: 'Javascrip'
       },
-      {
+      { 
+        id:2,
         descricao:'Matemática'
       },
-      {
+      { 
+        id:3,
         descricao: 'Economia'
       },
-      {
+      { 
+        id:4,
         descricao: 'Programação'
       },
-      {
+      { 
+        id:5,
         descricao: 'Gastronomia'
       },
-      {
+      { 
+        id:6,
         descricao: 'Filosofia'
       }
     ], {});
