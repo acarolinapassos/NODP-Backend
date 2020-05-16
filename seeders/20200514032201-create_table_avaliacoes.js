0e7d1ee44b0b5beb4f32acb3024ba052b82bdbbf
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-16 01:33:19 -0300

    seeders

--- seeders/20200514032201-create_table_avaliacoes.js
+++ seeders/20200514032201-create_table_avaliacoes.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('avaliacoes', [
-      {
+      { id:1,
         aula_id: 1,
         tipo_avaliacao: 1,
         nota:5
