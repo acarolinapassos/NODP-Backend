952c0bf66581d166785726df901d26ee1de02817
Erica Akira <Erica@MacBook-Air-de-Erica.local>
2020-05-15 21:24:25 -0300

    erica corrigindo migrations

--- migrations/20200513230445-create_table_canal_ensino.js
+++ migrations/20200513230445-create_table_canal_ensino.js
@@ -2,25 +2,25 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.createTable(
-        'canal_ensino',
-        {
-          id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-         },
-         descricao: {
-           type: Sequelize.STRING(45),
-           allowNull: false,
-            comment: "null",
-            unique: true
-         }
-        });
+    return queryInterface.createTable(
+      'canal_ensino', {
+        id: {
+          type: Sequelize.INTEGER(11),
+          primaryKey: true,
+          allowNull: false,
+          comment: "null",
+          autoIncrement: true
+        },
+        descricao: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+          comment: "null",
+          unique: true
+        }
+      });
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.dropTable('canal_ensino');
+    return queryInterface.dropTable('canal_ensino');
   }
 };
