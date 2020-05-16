fc12216e0aa2cf99af9a646b2cef3ca6593ef692
Alessandro Menezes <alessandroadm@live.com>
2020-05-15 22:37:48 -0300

    ajustando seeders

--- seeders/20200514032237-create_table_medalhas.js
+++ seeders/20200514032237-create_table_medalhas.js
@@ -1,25 +1,38 @@
+const moment = require('moment');
 'use strict';
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('medalhas', [{
+      id_post:1,
       postagens_id: 2,
-      remetente_id: 3
+      remetente_id: 3,
+      data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
+      id_post:1,
       postagens_id: 3,
-      remetente_id: 1
+      remetente_id: 1,
+      data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
+      id_post:1,
       postagens_id: 2,
-      remetente_id: 1
+      remetente_id: 1,
+      data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
+      id_post:1,
       postagens_id: 1,
-      remetente_id: 4
+      remetente_id: 4,
+      data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
+      id_post:1,
       postagens_id: 2,
-      remetente_id: 3
+      remetente_id: 3,
+      data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }, {
+      id_post:1,
       postagens_id: 5,
-      remetente_id: 4
+      remetente_id: 4,
+      data_hora: moment().format("YYYY, MM DD, h:mm:ss")
     }], {});
   },
 
--- seeders/20200514032304-create_table_anuncios.js
+++ seeders/20200514032304-create_table_anuncios.js
@@ -2,16 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('anuncios', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
    return queryInterface.bulkInsert('anuncios', [{
     descricao: "Não espere para se movimentar, venha para Bluefit" ,
     imagem: "default1.png",
@@ -43,13 +33,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-     
-    */
    return queryInterface.bulkDelete('anuncios', null, {});
   }
 };
