a4e7e2b61ae2e367416a718a28cdd0e846867548
André Oliveira <andreluisoliveira013@gmail.com>
2020-05-17 12:45:43 -0300

    adicionando rotas

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,70 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(`Deu certo aeeee`);
+router.get('/', function (req, res, next) {
+  res.render('entrar', { title: 'Login' });
 });
 
-router.post('/login', function(req, res, next) {
-<<<<<<< HEAD
-  res.send('Deu certo!')
-=======
-  res.send('Creu')
+
+/* GET index page. */
+router.get('/index', function (req, res, next) {
+  res.render('index', { title: 'Login' });
 });
 
-router.get('/sair', function(req, res, next) {
-  res.send('Creu')
->>>>>>> b5b4e7394b81bfa17c2b6d5eb51e12c20e1e9121
-})
+
+/* GET cadastro page. */
+router.get('/cadastro', function (req, res, next) {
+  res.render('cadastro', { title: 'Cadastro' });
+});
+
+/* GET home page. */
+router.get('/home', function (req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+
+/* GET perfil usuario id page. */
+router.get('/perfil-usuario/:id?', function (req, res, next) {
+  res.render('perfil-usuario', { title: 'Usuário' });
+});
+
+/* GET perfil page. */
+router.get('/perfil', function (req, res, next) {
+  res.render('perfil', { title: 'Perfil' });
+});
+
+/* GET pesquisas page. */
+router.get('/pesquisas', function (req, res, next) {
+  res.render('pesquisas', { title: 'Pesquisa' });
+});
+
+
+/* GET ranking alunos page. */
+router.get('/ranking-alunos', function (req, res, next) {
+  res.render('ranking-alunos', { title: 'Ranking' });
+});
+
+/* GET ranking professores page. */
+router.get('/ranking-professores', function (req, res, next) {
+  res.render('ranking-professores', { title: 'Ranking' });
+});
+
+/* GET apoiados/apoiando page. */
+router.get('/apoio', function (req, res, next) {
+  res.render('apoio', { title: 'Apoio' });
+});
+
+router.get('/apoiadores', function (req, res, next) {
+  res.render('apoiadores', { title: 'Apoiadores' });
+});
+
+/* GET notificacoes page. */
+router.get('/notificacoes', function (req, res, next) {
+  res.render('notificacoes', { title: 'Últimas notificações' });
+});
+
+/* GET mensagens page. */
+router.get('/mensagens', function (req, res, next) {
+  res.render('mensagens', { title: 'Últimas Mensagens' });
+});
 
 module.exports = router;
