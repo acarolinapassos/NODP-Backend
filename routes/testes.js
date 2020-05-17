fa8a788c592edeef4b8d53d9e5c5cf1dadd8164c
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-17 16:22:25 -0300

    metodo update de senha

--- routes/testes.js
+++ routes/testes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const UsuarioController = require('./../controllers/UsuarioController');
 
 router.get('/usuarios', UsuarioController.listar);
-router.post('/salvar-usuario', UsuarioController.salvar)
+router.post('/salvar-usuario', UsuarioController.salvar);
+router.put('/usuario',UsuarioController.editar)
 
 module.exports = router;
