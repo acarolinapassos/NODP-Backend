3959e9acde5ffe0404a067c64e0db57fcf540d48
André Oliveira <andreluisoliveira013@gmail.com>
2020-05-17 15:15:24 -0300

    Adicionando o controller de usuario

--- routes/testes.js
+++ routes/testes.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const UsuarioController = require('./../controllers/UsuarioController');
 
 router.get('/usuarios', UsuarioController.listar);
+router.post('/salvar-usuario', UsuarioController.salvar)
 
 module.exports = router;
