bc8bd6b3509723f289734591c4168e622a8428f5
Alessandro Menezes <alessandroadm@live.com>
2020-05-18 22:23:11 -0300

    implantando views

---
+++ readme/INSERTS_NODP.sql
@@ -0,0 +1,17 @@
+USE NODP_BD;
+INSERT INTO categorias_postagens(descricao) VALUES('ensinando'),
+('aprendendo'),('quero aprender'),('quero ensinar');
+INSERT INTO canal_ensino (descricao) VALUES('online'),
+('presencial'),('online/presencial');
+
+
+
+INSERT INTO tipos_avaliacoes (descricao) VALUES ('Professor'), ('Aluno'),
+('Aula');
+
+
+
+
+INSERT INTO tipos_notificacoes (descricao) VALUES ('Enviou mensagem'), ('Deu medalha na sua postagem'),
+('Deu moeda na sua postagem'), ('Apoiou você'), ('Comentou na sua postagem');
+SELECT * FROM canal_ensino;
