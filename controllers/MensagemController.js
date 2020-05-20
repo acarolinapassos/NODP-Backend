a8d36c633b60fc138eb20a1c7816597bca91231b
Alessandro Menezes <alessandroadm@live.com>
2020-05-20 18:54:57 -0300

    ajustando controller de mensagem

--- controllers/MensagemController.js
+++ controllers/MensagemController.js
@@ -1,31 +1,36 @@
 const { Mensagem } = require('../models')
 
 module.exports = {
-
-    listarMensagens: async(req, res, next) => {
-        try{
-            let { destinatario, usuario } = req.body;
-            let resposta = await Mensagem.findAll( {where: {usuario_id: usuario, destinatario_id: destinatario}}) 
-            res.send(resposta)
+    //-------------------------------------------------------------------------
+    //Listar mensagens entre usuários : GET > query = usuario, destinatario
+    //http://localhost:3000/teste/listar-mensagens?usuario=1&destinatario=2
+    listarMensagens: async (req, res, next) => {
+        try {
+            let { destinatario, usuario } = req.query;
+            let resposta = await Mensagem.findAll({ where: { usuario_id: usuario, destinatario_id: destinatario } });
+            res.send(resposta);
         }
-        catch (error){
-            console.log(error)
+        catch (error) {
+            console.log(error);
         }
-
+        
     },
-    adicionarMensagem: async(req, res, next) => {
-        try{
+    //-------------------------------------------------------------------------
+    //Listar mensagens entre usuários : POST > body = usuario, destinatario, mensagem
+    //http://localhost:3000/teste/enviar-mensagem
+    adicionarMensagem: async (req, res, next) => {
+        try {
             let { usuario, destinatario, mensagem } = req.body;
             let objeto = {
                 usuario_id: usuario,
                 destinatario_id: destinatario,
                 mensagem: mensagem
-            }
-            let resposta = await Mensagem.create(objeto)
-            res.send(resposta)
+            };
+            let resposta = await Mensagem.create(objeto);
+            res.send(resposta);
         }
-        catch(error){
-            console.log(error)
+        catch (error) {
+            console.log(error);
         }
     }
-}
+};
