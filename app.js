d495e263e44597d89ebde4651a5ca9ce0d7ddbc5
Alessandro Menezes <alessandroadm@live.com>
2020-05-20 23:43:57 -0300

    autenticação

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieSession = require('cookie-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const testeRouter = require('./routes/testes');
+const auth = require('./middleware/auth');
 
 var app = express();
 
@@ -20,9 +22,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieSession({
+  name: 'session',
+  keys: ['JK@#G654/34FF3$F&*0'],
+  maxAge: 60 * 60 * 24 * 7
+}));
+
 app.use('/', indexRouter);
 app.use('/teste', testeRouter);
-app.use('/users', usersRouter);
+app.use('/users', auth.autenticar, usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
