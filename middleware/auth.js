15e89f483d511cccbc8731b06c65ce69f0ef47f2
Alessandro Menezes <alessandroadm@live.com>
2020-05-21 00:23:10 -0300

    aut

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ module.exports = {
   autenticar: (req, res, next) => {
     const LOGADO = req.session.USER != undefined;
     if (LOGADO) {
+      res.locals.USER = req.session.USER;
       next();
     } else {
       res.redirect('/');
