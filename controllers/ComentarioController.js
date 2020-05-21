c726a3f1ec1b33b62e86d8ac10a938dcef507725
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-20 22:55:23 -0300

    crud dos comentarios

---
+++ controllers/ComentarioController.js
@@ -0,0 +1,70 @@
+const {Comentario} = require('./../models');
+
+// listar comentarios
+module.exports={
+listar: async (req,res,next) =>{
+    try {
+        let comentarios = await Comentario.findAll({
+
+        });
+        res.send(comentarios);
+    } catch (error) {
+            console.log(error);
+          }
+    },
+
+    salvar:async (req,res) =>{
+        try {
+            let {
+                texto,
+                usuario_id,
+                post_id
+            } = req.body;
+        const salvar = await Comentario.create({
+            texto,
+            usuario_id,
+            post_id
+        }); 
+        res.send('Comentário enviado')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    editar: async (req,res) =>{
+        try {
+            let {
+                id,
+                texto,
+                usuario_id,
+                post_id
+            } = req.body;
+        const editar = await Comentario.update({
+            texto,
+           
+        }, {where:{id:id}}); 
+        res.send('Comentário editado')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    excluir: async (req,res) =>{
+        try {
+            
+            let {
+                id
+            } = req.body;
+
+            const excluir = await Comentario.destroy({ where: { id } }
+                );
+                res.send('Post excluído');
+              } catch (error) {
+                console.log(error);
+                
+              }
+            },
+    }
+
+
+
