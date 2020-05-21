34d68944ac51bf3ce348fbd7a2688eb0f4af7f13
acarolinapassos <acarolinapassos@yahoo.com.br>
2020-05-21 12:47:12 -0300

    crud das notificacoes 100% concluída

--- controllers/NotificacaoController.js
+++ controllers/NotificacaoController.js
@@ -1,8 +1,8 @@
-const {
-    Notificacao
-} = require('../models');
+const {Notificacao} = require('../models');
 
 module.exports = {
+// listar notificacoes
+//http://localhost:3000/teste/notificacoes
     listar: async (req, res, next) => {
         try {
             let notificacoes = await Notificacao.findAll({
@@ -13,6 +13,8 @@ module.exports = {
             console.log(error);
         }
     },
+// salvar notificacoes
+//http://localhost:3000/teste/notificacoes
     salvar: async (req, res) => {
         try {
             let {
@@ -34,6 +36,31 @@ module.exports = {
             console.log(error)
         }
     },
+// editar o atributo 'lida' das notificacoes
+//http://localhost:3000/teste/notificacoes
+    editar: async (req,res) =>{
+        try {
+            
+            let {
+                id,
+                descricao,
+                tipo_notificacao_id,
+                usuario_id,
+                remetente_id,
+                lida
+            } = req.body;
+
+            const editar = await Notificacao.update({lida},
+                { where: { id:id } }
+                );
+                res.send('Notificação lida');
+              } catch (error) {
+                console.log(error);
+                
+              }
+            },
+// excluir notificacoes
+//http://localhost:3000/teste/notificacoes
     excluir: async (req,res) =>{
         try {
             
@@ -48,5 +75,5 @@ module.exports = {
                 console.log(error);
                 
               }
-            },
+            }
 }
