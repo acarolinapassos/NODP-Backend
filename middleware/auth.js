7d020c5a93a88e35d62c94539f3f532c48b19c9a
Alessandro Menezes <alessandroadm@live.com>
2020-05-22 15:53:46 -0300

    finalizado autenticacao

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 module.exports = {
+  //Verificar se o usuário está logado
   autenticar: (req, res, next) => {
     const LOGADO = req.session.USER != undefined;
     if (LOGADO) {
@@ -7,5 +8,25 @@ module.exports = {
     } else {
       res.redirect('/');
     }
+  },
+
+  //Fazer logoff no sistema
+  sair: (req, res, next) => {
+    req.session.USER = undefined;
+    res.locals.USER = undefined;
+    res.redirect('/login');
+  },
+
+  //Salvar a sessão do usuário
+  salvarSessao: (req, res, next, user) => {
+    //Salvar usuário na sessão
+    req.session.USER = {
+      id: user.id,
+      admin: user.admin,
+      email: user.email,
+      ativo: user.ativo
+    };
+    res.locals.USER = req.session.USER;
+    res.redirect('/home');
   }
 };
