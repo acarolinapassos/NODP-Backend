715d8bdd7fa0d736b78da3a32ae6da36caf5945b
Erica Akira <Erica@MacBook-Air-de-Erica.local>
2020-05-27 21:47:09 -0300

    erica

--- public/js/fetch-post.js
+++ public/js/fetch-post.js
@@ -1,14 +1,15 @@
-async function salvarMedalhas (event) {
-    console.log(event)
-    // try {
-    //     const promise = await fetch ("/users/medalhas" , {
-    //         body:{post},
-    //         method:'POST',
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         }
-    //     })
-    // } catch (error) {
-    //     console.log(error)
-    // }
+async function salvarMedalhas (element) {
+    //console.log(element.dataset.post_id);
+    let id = element.dataset.id_post;
+    try {
+        const promise = await fetch ("/users/medalhas" , {
+            body:JSON.stringify({id_post: id}),
+            method:'POST',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
