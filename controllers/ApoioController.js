437a511cabe8ee7d17cfc7d878dfed8c036c2be4
Alessandro Menezes <alessandroadm@live.com>
2020-05-29 15:44:56 -0300

    listando apoiadores

--- controllers/ApoioController.js
+++ controllers/ApoioController.js
@@ -2,17 +2,17 @@ const { Apoio, Perfil } = require('./../models');
 module.exports = {
   apoiar: async (req, res) => {
     try {
-
+      
       let { apoiador_id, apoiado_id} = req.body;
       let salvar = await Apoio.create(apoiador_id, apoiado_id);
       res.status(200);
-
+      
     } catch (error) {
       res.status(401).json({error:'Não foi possível salvar'});
     }
   },
-
-
+  
+  
   listarApoiados: async (req, res) => {
     try {
       let apoiados = await Apoio.findAll({
@@ -25,13 +25,18 @@ module.exports = {
       console.log(error);
     }
   },
-
-
+  
+  
   listarApoiadores: async (req, res) => {
     try {
-      
+      let apoiadores = await Apoio.findAll({
+        where: {
+          apoiado_id: req.sessio.USER.id
+        }
+      });
+      res.send(apoiadores);
     } catch (error) {
-      
+      console.log(error);
     }
   }
 };
