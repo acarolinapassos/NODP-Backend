3cd668d826d35eb2d150c906a77bed3f26607b7b
André Oliveira <andreluisoliveira013@gmail.com>
2020-05-28 22:45:08 -0300

    Adicionando listagem de mensagem da conversa

---
+++ public/js/requisitarMensagens.js
@@ -0,0 +1,87 @@
+state = {
+    usuario: ''
+}
+
+async function listarMensagens (element) {
+
+    let id = element.dataset.idmensagemusuario;
+    state.usuario = id
+    try {
+        const promise = await fetch(`/users/listarMensagens?id=${id}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        if (!promise.ok) {
+            alert("Error");
+            return;
+       }
+      
+       return promise.json()
+       
+    } catch(error) {
+        console.log(error)
+    }
+
+ }
+
+async function enviar (element) {
+
+    const respostas = await listarMensagens(element)
+
+    let ul = document.querySelector(".informacoes-do-msg-mensagens-ul")
+    let header = document.querySelector(".msg-feed-header")
+    let lis = []
+
+    for (let resposta of respostas) {
+         lis.push(`
+         <li class="msg">
+         <h2 class="subtitulo">
+             ${resposta.perfil_msg.nome}
+         </h2>
+         <cite class="texto">
+             ${resposta.mensagem}
+         </cite>
+         <time class="horario">
+             ${resposta.data_hora}
+         </time>
+     </li>`)
+    }
+
+    ul.innerHTML = lis
+    header.innerHTML = `
+    <header class="msg-feed-header">
+    <div class="msg-feed-header-avatar">
+      <img src="/img/profile-img/${respostas[0].perfil_msg.avatar}" alt="">
+    </div>
+    <div>
+      <h1 class="user-name subtitulo">${respostas[0].perfil_msg.nome}</h1>
+    </div>
+  </header>`
+}
+
+
+async function enviarMensagem () {
+    let mensagem = document.querySelector('.texto-msg').textContent
+    try {
+        const promise = await fetch('/users/mensagens', {
+            body: JSON.stringify({
+                destinatario: state.usuario,
+                mensagem: mensagem
+            }),
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        if (!promise.ok) {
+            alert("Error");
+            return;
+       }
+       
+    } catch(err){
+        console.log(err)
+    }
+
+}
