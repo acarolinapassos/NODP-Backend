09313ac7cb8e468c0f20c7fbc6daf063c70bda0a
Alessandro Menezes <alessandroadm@live.com>
2020-05-29 01:06:34 -0300

    finalizado pesquisar e correção no post feed

--- public/js/pesquisar.js
+++ public/js/pesquisar.js
@@ -1,11 +1,23 @@
-const FILTRO = document.querySelector('#filtroPesquisa');
+const FILTRO = document.getElementById('filtroPesquisa');
+const INPUT_SEARCH = document.querySelector('input[type=search');
+const LUPA = document.getElementById('form-pesquisar');
 
-async function filtrar() {
+
+async function filtrar(element) {
   try {
-    
-    console.log(FILTRO);
-
+    var query = element.dataset.item;
+    if (element.classList.value != 'botao ativo') {
+      location.href = `/users/pesquisas?search=pesquisar&descricao=${INPUT_SEARCH.value}&${query}`;
+    }
   } catch (error) {
-    
+    console.log(error);
   }
 }
+
+function pesquisar() {
+  location.href = `/users/pesquisas?search=pesquisar&descricao=${INPUT_SEARCH.value}&usuario=1&aprender=3&ensinar=4&ensinando=1&aprendendo=2`;
+}
+
+
+LUPA.addEventListener('submit', pesquisar);
+
