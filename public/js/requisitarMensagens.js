79f313040d22b61c865f75f639cc32d67a00b014
André Oliveira <andreluisoliveira013@gmail.com>
2020-06-05 16:17:47 -0300

    Atualizando

--- public/js/requisitarMensagens.js
+++ public/js/requisitarMensagens.js
@@ -81,41 +81,6 @@ async function listarMensagens (element) {
   </header>`;
 }
 
-async function enviar (element) {
-    
-    const respostas = await listarMensagens(element);
-
-    let ul = document.querySelector(".informacoes-do-msg-mensagens-ul");
-    let header = document.querySelector(".msg-feed-header");
-    let lis = '';
-
-    for (let resposta of respostas) {
-        lis +=`
-    <li class="msg">
-         <h2 class="subtitulo">
-             ${resposta.perfil_msg.nome}
-         </h2>
-         <cite class="texto">
-             ${resposta.mensagem}
-         </cite>
-         <time class="horario">
-             ${resposta.data_hora}
-         </time>
-    </li>`;
-    }
-
-    ul.innerHTML = lis;
-    header.innerHTML = `
-    <header class="msg-feed-header">
-    <div class="msg-feed-header-avatar">
-      <img src="/img/profile-img/${respostas[0].perfil_msg.avatar}" alt="">
-    </div>
-    <div>
-      <h1 class="user-name subtitulo">${respostas[0].perfil_msg.nome}</h1>
-    </div>
-  </header>`;
-}
-
 
 async function enviarMensagem () {
     let mensagem = document.querySelector('.texto-msg').value
