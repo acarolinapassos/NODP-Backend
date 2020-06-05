01afe452b6d50eb27e6ed2a54a1073dfb5dd941e
Alessandro Menezes <alessandroadm@live.com>
2020-06-05 13:17:58 -0300

    corrigido exibição do modal ao realizar login

--- public/js/primeiro-cadastro.js
+++ public/js/primeiro-cadastro.js
@@ -1,7 +1,7 @@
 if (location.pathname=="/cadastro"){
 let form = document.getElementById('form-cadastro');
 
-form.addEventListener('click', function () {
-  localStorage.setItem('nodp_key', '1');
-})
+  form.addEventListener('submit', function () {
+    localStorage.setItem('nodp_key', '1');
+  });
 }
