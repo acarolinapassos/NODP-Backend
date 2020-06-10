bf958af1cf8c035029bba7a49babfae6c364a540
Alessandro Menezes <alessandroadm@live.com>
2020-06-09 22:45:14 -0300

    adicionando limitador de caracteres mensagens

--- public/js/scroll-users-mensagens.js
+++ public/js/scroll-users-mensagens.js
@@ -4,32 +4,41 @@ var DIREITA = document.querySelectorAll('.anterior');
 var ESQUERDA = document.querySelectorAll('.proximo');
 var VIEW_USERS = document.querySelectorAll('.msg-ul-amigos');
 
-  //Esta função aplica o efeito no botão clicado pelo usuário para adicionar filtro do usuário
-  for (let user of ulUsuarios) {
-    user.addEventListener('click', function (e) {
-      var clicou = e.target.parentNode.parentNode;
-      if (clicou.tagName == 'LI') {
-        clicou.classList.toggle('usuario-selecionado');
-      }
-    });
-  }
+//Esta função aplica o efeito no botão clicado pelo usuário para adicionar filtro do usuário
+for (let user of ulUsuarios) {
+  user.addEventListener('click', function (e) {
+    var clicou = e.target.parentNode.parentNode;
+    if (clicou.tagName == 'LI') {
+      clicou.classList.toggle('usuario-selecionado');
+    }
+  });
+}
 
-  for (let btn of ESQUERDA) {
-    btn.addEventListener('click', function() {
-      if (VIEW_USERS[1] != undefined) {
-        VIEW_USERS[1].scrollTo(VIEW_USERS[1].offsetWidth + VIEW_USERS[1].offsetWidth, 0);
-      } else {
-        VIEW_USERS[0].scrollTo(VIEW_USERS[0].offsetWidth + VIEW_USERS[0].offsetWidth, 0);
-      }
-    });
-  }
+for (let btn of ESQUERDA) {
+  btn.addEventListener('click', function() {
+    if (VIEW_USERS[1] != undefined) {
+      VIEW_USERS[1].scrollTo(VIEW_USERS[1].offsetWidth + VIEW_USERS[1].offsetWidth, 0);
+    } else {
+      VIEW_USERS[0].scrollTo(VIEW_USERS[0].offsetWidth + VIEW_USERS[0].offsetWidth, 0);
+    }
+  });
+}
 
-  for (let btn of DIREITA) {
-    btn.addEventListener('click', function () {
-      if (VIEW_USERS[1] != undefined) {
-        VIEW_USERS[1].scrollTo(VIEW_USERS[1].offsetWidth - VIEW_USERS[1].offsetWidth, 0);
-      } else {
-        VIEW_USERS[0].scrollTo(VIEW_USERS[0].offsetWidth - VIEW_USERS[0].offsetWidth, 0);
-      }
-    });
-  }
+for (let btn of DIREITA) {
+  btn.addEventListener('click', function () {
+    if (VIEW_USERS[1] != undefined) {
+      VIEW_USERS[1].scrollTo(VIEW_USERS[1].offsetWidth - VIEW_USERS[1].offsetWidth, 0);
+    } else {
+      VIEW_USERS[0].scrollTo(VIEW_USERS[0].offsetWidth - VIEW_USERS[0].offsetWidth, 0);
+    }
+  });
+}
+
+
+
+//Atualizar limite de caracteres no modal publicação
+function atualizarLimitCaracteresMsg(el, idElemento) {
+  var labelLimit = document.getElementById(idElemento);
+  var qtdCaracteresDigitados = parseInt(el.value.length);
+  labelLimit.textContent = 250 - qtdCaracteresDigitados;
+}
