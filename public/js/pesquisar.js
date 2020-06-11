4042e67eb3d3642733ea841b8f685c7a92e4f3fe
Alessandro Menezes <alessandroadm@live.com>
2020-06-11 16:41:39 -0300

    resolvendo bug de pesquisa

--- public/js/pesquisar.js
+++ public/js/pesquisar.js
@@ -2,6 +2,7 @@ const FILTRO = document.getElementById('filtroPesquisa');
 const INPUT_SEARCH = document.querySelector('input[type=search');
 const INPUT_SEARCH_MOBILE = document.getElementById('search-mobile');
 const LUPA = document.getElementById('form-pesquisar');
+const LUPA_MOBILE = document.getElementById('form-pesquisar_mobile');
 
 
 async function filtrar(element) {
@@ -20,9 +21,15 @@ async function filtrar(element) {
 }
 
 function pesquisar() {
+  event.preventDefault();
   location.href = `/users/pesquisas?search=pesquisar&descricao=${INPUT_SEARCH.value}&usuario=1&aprender=3&ensinar=4&ensinando=1&aprendendo=2`;
+}
+function pesquisar_mobile() {
+  event.preventDefault();
+  location.href = `/users/pesquisas?search=pesquisar&descricao=${INPUT_SEARCH_MOBILE.value}&usuario=1&aprender=3&ensinar=4&ensinando=1&aprendendo=2`;
 }
 
 
 LUPA.addEventListener('submit', pesquisar);
+LUPA_MOBILE.addEventListener('submit', pesquisar_mobile);
 
